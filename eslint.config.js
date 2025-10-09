e70b70e0cb5dedc1a50afc5b242a789640278851
Marcos <macorman.06@gmail.com>
2025-10-09 18:58:02 +0200

    update: ESLint remove warning any

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,7 @@ export default tseslint.config(
           varsIgnorePattern: '^_',
         },
       ],
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       'prefer-const': 'error',
       'no-var': 'error',
